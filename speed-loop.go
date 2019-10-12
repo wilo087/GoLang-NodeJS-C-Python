db53f2e118a397e768c2e4acdb3f54b24df44dd1
Wilowayne De La Cruz G <wilo0087@gmail.com>
2019-10-12 16:40:52 -0400

    Adding go test

---
+++ speed-loop.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main()  {
+    start := time.Now()
+    for i := 1; i < 1000000000; i++ {
+        // Empty lopp
+    }
+    end := time.Now()
+
+    elapsed := end.Sub(start)
+    
+    fmt.Println(elapsed)
+}
