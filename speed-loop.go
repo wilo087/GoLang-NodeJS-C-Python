42d2b3bcce8a43485655ce1dd1ee2c6f17eb966f
Wilowayne De La Cruz G <wilo0087@gmail.com>
2019-10-12 16:41:52 -0400

    Fixing go loop

--- speed-loop.go
+++ speed-loop.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
-func main()  {
-    start := time.Now()
-    for i := 1; i < 1000000000; i++ {
-        // Empty lopp
-    }
-    end := time.Now()
+func main() {
+	start := time.Now()
+	for i := 1; i < 1000000000; i++ {
+		// Empty lopp
+	}
+	end := time.Now()
 
-    elapsed := end.Sub(start)
-    
-    fmt.Println(elapsed)
+	elapsed := end.Sub(start)
+
+	fmt.Println(elapsed)
 }
